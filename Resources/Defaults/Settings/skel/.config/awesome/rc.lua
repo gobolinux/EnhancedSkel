7b8cd56136be7abe2a75b23f6eb1d4d47d0ec4e9
Lucas C. Villa Real <lucasvr@gmail.com>
2020-02-19 15:22:40 -0300

    Rename methods deprecated by Awesome

--- Resources/Defaults/Settings/skel/.config/awesome/rc.lua
+++ Resources/Defaults/Settings/skel/.config/awesome/rc.lua
@@ -189,7 +189,7 @@ menubar.utils.terminal = terminal -- Set the terminal for applications that requ
 
 -- Wibox
 -- Create a textclock widget
-local mytextclock = awful.widget.textclock(nil, 5)
+local mytextclock = wibox.widget.textclock(nil, 5)
 
 -- Create a wibox for each screen and add it
 local mywibox = {}
@@ -259,7 +259,7 @@ for s = 1, screen.count() do
    mytasklist[s] = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, mytasklist.buttons)
    
    -- Create the wibox
-   mywibox[s] = awful.wibox({ position = "top", screen = s })
+   mywibox[s] = awful.wibar({ position = "top", screen = s })
    
    -- Widgets that are aligned to the left
    local left_layout = wibox.layout.fixed.horizontal()
