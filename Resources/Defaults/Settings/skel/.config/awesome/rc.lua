fe9e96cc80f21871d349b3acc212a26f335944e3
Lucas C. Villa Real <lucasvr@gmail.com>
2020-03-12 16:29:06 -0300

    Prefix xft: so that the right font is picked..

--- Resources/Defaults/Settings/skel/.config/awesome/rc.lua
+++ Resources/Defaults/Settings/skel/.config/awesome/rc.lua
@@ -45,8 +45,8 @@ local beautiful = require("beautiful")
 beautiful.init("~/.config/awesome/themes/neon/theme.lua")
 
 awful.titlebar.enable_tooltip = false
-hotkeys_popup.title_font = "Lode Sans Mono Bold 12"
-hotkeys_popup.description_font = "Lode Sans Mono 12"
+hotkeys_popup.title_font = "xft:Lode Sans Mono Bold-12"
+hotkeys_popup.description_font = "xft:Lode Sans Mono-12"
 hotkeys_popup.group_margin = 20
 
 -- Variable definitions
