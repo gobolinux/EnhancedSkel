2507252d49a77437449e3bb7c140082a259c0135
Hisham Muhammad <hisham@gobolinux.org>
2023-05-17 18:45:10 -0300

    pressing PrintScr twice cancels the screenshot

--- Resources/Defaults/Settings/skel/.config/awesome/rc.lua
+++ Resources/Defaults/Settings/skel/.config/awesome/rc.lua
@@ -328,7 +328,11 @@ root.buttons(awful.util.table.join(
 
 local function screenshot()
    local name = os.getenv("HOME") .. "/screenshot-" .. os.date("%Y-%m-%d-%H-%M-%S") .. ".png"
-   os.execute("import "..name.."; cat "..name.." | xclip -i -selection clipboard -t image/png")
+
+   os.execute([[ps aux | grep '[i]mport' && killall import || {
+      ( import ~/]]..name.." && cat  ~/"..name..[[ | xclip -i -selection clipboard -t image/png ) &
+   }]])
+
    naughty.notify({ preset = naughty.config.presets.normal,
                     title = "Screen captured",
                     text = "Screenshot saved to"..name})
